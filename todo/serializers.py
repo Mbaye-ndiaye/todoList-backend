e12d43ef7531fe22d95830a2a9a6813f2754d690
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-11-20 17:32:20 +0000

    creer require

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
+    # creer le model serail 
     class Meta:
         model=Todo
         fields='__all__'
