14e80825a5514a34565c7abcc56e19b3ad947deb
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-11-21 09:07:37 +0000

    djangorestframework

--- backend/settings.py
+++ backend/settings.py
@@ -58,14 +58,7 @@ CORS_ALLOWED_ORIGINS = [
     
 ]
 
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
+
 CORS_ORIGIN_ALLOW_ALL= True
 ROOT_URLCONF = 'backend.urls'
 
