13ef36b32d32ef37d29fa8fc2b441d8af3778a08
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-12-08 17:06:23 +0000

    verifier la version drf-yasg

--- backend/urls.py
+++ backend/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('todo.urls')),
-    path('swagger/', schema_view.as_view(), name='swagger'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),  
     # Route pour afficher swagger UI
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swaager-ui'),
     # Route pour afficher la documentation Redoc
